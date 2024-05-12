4b3ecd53ae75ff15d98243205a3ad63073d968a1
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-05-12 15:43:41 +0800

    first

---
+++ src/main/java/com/recruitment/recruitmentchat/RecruitmentChatApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.recruitmentchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitment/recruitmentchat/RecruitmentChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.recruitmentchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
